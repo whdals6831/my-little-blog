dc993e542da35b64b3e93cd78be2f9f98e3613f2
stupidbox <whdals6831@gmail.com>
2026-03-15 18:10:40 +0900

    feat : router 예시 추가

--- src/views/main/ui/main-page.tsx
+++ src/views/main/ui/main-page.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@/shared/ui/shadcn/button';
+import Link from 'next/link';
 
 export function MainPage() {
   return (
     <>
       <div>캬캬캬!</div>
-      <Button>음하하하하!</Button>
+      <Button asChild>
+        <Link href={'/post'}>글로 이동</Link>
+      </Button>
     </>
   );
 }
---
+++ src/views/post/ui/post-page.tsx
@@ -0,0 +1,10 @@
+import { Input } from '@/shared/ui/shadcn/input';
+
+export function PostPage() {
+  return (
+    <>
+      <div>안녕 나는 글이야.</div>
+      <Input placeholder="안녕!"></Input>
+    </>
+  );
+}
